1b074ea8dad52103efa13da8457652fe8775dec1
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-06 23:07:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('*');
+}
+
+function increaseRankBy(n){
+  var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  
+  for (let i=0; i<lis.length; i++){
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML)+n).toString()
+  }
+}
+
+function deepestChild(){
+  var curr = document.getElementById('grand-node').querySelectorAll('div');
+  var next = [];
+  
+  while (curr.length>0){
+    next = curr[0].children;
+    for (let i=0; i<next.length; i++){
+      curr.push(next.childNodes[i]);
+    }
+  }
+  
+  return curr;
+}
